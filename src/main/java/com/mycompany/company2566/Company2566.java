c2056bc244cd3022445a64c78644e6c4d2889220
LnwMosDev <natdanai1170.com@gmail.com>
2024-02-21 21:39:58 +0700

    basic store Java

---
+++ src/main/java/com/mycompany/company2566/Company2566.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.company2566;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Company2566 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
